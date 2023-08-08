5fbf533043f641424a8b76fcfa4cb0c9374c133c
Jena Donlin <donlinje@gmail.com>
2023-08-07 17:10:50 -0700

    Making json readable

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# Make the json readable
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Creates a container for repeat readable content
+streamlit.dataframe(fruityvice_normalized)
